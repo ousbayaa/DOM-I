e22958eac7f7fe243e4cb8e3a9f4e88df272f8c5
Ous Bayaa <bayaaous@gmail.com>
2020-02-10 18:35:01 -0800

    added nav bar and image

--- js/index.js
+++ js/index.js
@@ -40,3 +40,17 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let links = document.querySelectorAll('a');
+links[0].textContent = siteContent["nav"]["nav-item-1"];
+links[1].textContent = siteContent["nav"]["nav-item-2"];
+links[2].textContent = siteContent["nav"]["nav-item-3"];
+links[3].textContent = siteContent["nav"]["nav-item-4"];
+links[4].textContent = siteContent["nav"]["nav-item-5"];
+links[5].textContent = siteContent["nav"]["nav-item-6"];
+
+let ctaImg = document.getElementById('cta-img');
+ctaImg.src = siteContent["cta"]["img-src"];
+
+
+
